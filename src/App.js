d60aef94fb90b9e7b65348711c8c21c30762dc6c
lukcy <lukcy@gmail.com>
2025-01-25 10:38:13 +0530

    Modified Code

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 import React from "react";
-import { Routes , Route, BrowserRouter, } from 'react-router-dom'
+import { Routes, Route, BrowserRouter, } from 'react-router-dom'
 
 import Signin from "./components/Signin ";
 import Signup from "./components/Signup";
 import Home from "./components/Home";
 import Forgetpass from "./components/Forgetpass";
 import Validate from "./components/Validate";
+
 function App() {
   return (
-   <>
- <BrowserRouter>
- <Routes>
-  <Route path="/" element={<Home/>}/>
-  <Route path="/validate" element={<Validate/>}/>
-<Route path="/register" element={<Signup/>}/> 
-<Route path="/login" element={<Signin/>}/> 
-<Route path="/forgotpass" element={<Forgetpass/>}/> 
- </Routes>
- </BrowserRouter>
-   </>
+    <>
+      <BrowserRouter>
+        <Routes>
+          
+          <Route path="/" element={<Home />} />
+          <Route path="/validate" element={<Validate />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/login" element={<Signin />} />
+          <Route path="/forgotpass" element={<Forgetpass />} />
+        </Routes>
+      </BrowserRouter>
+    </>
 
   );
 }

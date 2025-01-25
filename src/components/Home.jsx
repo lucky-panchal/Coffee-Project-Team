904a7e9d9e087735a575ba6023022af2bbcced34
lukcy <lukcy@gmail.com>
2025-01-25 10:36:45 +0530

    Modified Code

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,19 +1,16 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import './Home.css'; 
 
 const Home = () => {
-
   const navigate = useNavigate();
-    
+
   return (
-    <div>
-        <button onClick={() => navigate('/register')}>Register</button>
-      
-      <br />
-      
-      <button onClick={() => navigate('/login')}>Login</button>
+    <div className="container"> {/* Use the container class */}
+      <button className="button" onClick={() => navigate('/register')}>Register</button>
+      <button className="button" onClick={() => navigate('/login')}>Login</button>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;

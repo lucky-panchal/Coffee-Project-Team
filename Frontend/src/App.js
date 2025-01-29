ce0e9c46b1874b84037d137f7803924955545fed
Ronak <itzronakmalam94@gmail.com>
2025-01-29 11:40:51 +0530

    Merged two repo's from old project

---
+++ Frontend/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Routes, Route, BrowserRouter, } from 'react-router-dom'
+
+import Signin from "./components/Signin ";
+import Signup from "./components/Signup";
+import Home from "./components/Home";
+import Forgetpass from "./components/Forgetpass";
+import Validate from "./components/Validate";
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          
+          <Route path="/" element={<Home />} />
+          <Route path="/validate" element={<Validate />} />
+          <Route path="/register" element={<Signup />} />
+          <Route path="/login" element={<Signin />} />
+          <Route path="/forgotpass" element={<Forgetpass />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+
+  );
+}
+
+export default App;

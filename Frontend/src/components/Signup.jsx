b051b7b8779ef8338c047af8424061a273aa1ae0
lucky-panchal <luckykanit31122006@gmail.com>
2025-02-05 14:35:44 +0530

    Signup and Signin css

--- Frontend/src/components/Signup.jsx
+++ Frontend/src/components/Signup.jsx
@@ -3,7 +3,7 @@ import { useForm } from 'react-hook-form';
 import { ToastContainer, toast } from 'react-toastify';
 import "react-toastify/dist/ReactToastify.css";
 import { useNavigate } from 'react-router-dom';
-import './Signup.css'; 
+import './Signup.css';
 
 function Signup() {
   const { register, handleSubmit, formState: { errors } } = useForm();
@@ -25,8 +25,8 @@ function Signup() {
         toast.success(`${resData.message}`, {
           position: "top-right"
         });
-       
-        navigate('/login'); 
+
+        navigate('/login');
       } else {
         toast.error(`${resData.error}`, {
           position: "top-right"
@@ -41,41 +41,48 @@ function Signup() {
   }
 
   return (
-    <div className="container"> 
-      <form onSubmit={handleSubmit(onSubmit)}>
-        <input
-          {...register('email', {
-            required: "Email is required",
-            pattern: {
-              value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
-              message: "Invalid email format"
-            }
-          })}
-          type="text"
-          placeholder="Email..."
-        />
-        {errors.email && <p>{errors.email.message}</p>}
+    <>
+      <div className='h2'><h2>Register</h2></div>
+      <div className="signup">
 
-        <input
-          {...register('password', {
-            required: "Password is required",
-            minLength: {
-              value: 6,
-              message: "Password must be at least 6 characters"
-            }
-          })}
-          type="password"
-          placeholder="Password..."
-        />
-        {errors.password && <p>{errors.password.message}</p>}
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <input
+className="userbox"
+            {...register('email', {
+              required: "Email is required",
+              pattern: {
+                value: /^[^\s@]+@[^\s@]+\.[^\s@]+$/,
+                message: "Invalid email format"
+              }
+            })}
+            type="text"
+            placeholder="Email..."
+          />
+          {errors.email && <p>{errors.email.message}</p>}
 
-        <span onClick={() => navigate('/login')} >Do You Have Already An Account?</span>
-        <button type="submit">
-          Register
-        </button>
-        <ToastContainer />
-      </form>
-    </div>
+          <input
+          className="userbox"
+            {...register('password', {
+              required: "Password is required",
+              minLength: {
+                value: 6,
+                message: "Password must be at least 6 characters"
+              }
+            })}
+            type="password"
+            placeholder="Password..."
+          />
+          {errors.password && <p>{errors.password.message}</p>}
+
+          <span onClick={() => navigate('/login')} >Do You Have Already An Account?</span>
+          <button type="submit">
+            Register
+          </button>
+          <ToastContainer />
+        </form>
+      </div>
+    </>
+
   );
 }
 

2f7c90084cbbbbec638eae300cf7c72761284bed
anmo2834 <anmolsinha4321@gmail.com>
2025-02-05 14:39:35 +0530

    change the alignment and icon of the back button in filter component

--- Frontend/src/components/filter/Filter.jsx
+++ Frontend/src/components/filter/Filter.jsx
@@ -9,7 +9,7 @@ function Filter() {
       return;
     }
     const filter = document.querySelector('.filter-contain');
-    if(filter.style.display === 'none') {
+    if (filter.style.display === 'none') {
       filter.style.display = 'flex';
     } else {
       filter.style.display = 'none';
@@ -22,10 +22,12 @@ function Filter() {
   }
 
   return (
-    <div onClick={handleHideFilter} className='filter-contain' style={{display: 'none'}}>
+    <div onClick={handleHideFilter} className='filter-contain' style={{ display: 'none' }}>
       <div className="filter-box">
         <div className="filter-nav">
-          <span onClick={handleBack} className='goBackBtn'><svg xmlns="http://www.w3.org/2000/svg" height="40px" viewBox="0 -960 960 960" width="40px" fill="brown"><path d="M560-240 320-480l240-240 56 56-184 184 184 184-56 56Z"/></svg></span>
+          <span onClick={handleBack} className='goBackBtn'>
+            <svg xmlns="http://www.w3.org/2000/svg" height="30px" viewBox="0 -960 960 960" width="30px" fill="#e8eaed"><path d="m256-200-56-56 224-224-224-224 56-56 224 224 224-224 56 56-224 224 224 224-56 56-224-224-224 224Z" /></svg>
+          </span>
           <h1>filter</h1>
         </div>
       </div>
